18ee03a16b67d27a2934f77b1778e97f7db91d7a
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-12-15 16:06:22 +0100

    presentation slides

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # used-car-liquidity
+
+The manuscript of the article can be fully reproduces after running the contained `codes`:
+
+1. `utils.R` packages and functions
+
+2. `copy_data.R`
+
+To knit the manuscript, knit it in RStudio & run the `fig_labels.R` script to modify the manuscript.tex & manuscript.pdf files.
+
--- codes/copy_data.R
+++ codes/copy_data.R
@@ -1,5 +1,9 @@
+# copy the raw data from another local repository
+# securing that we download daily the data and we do not touch that repo by mistake
+# you may also copy that by hand
+
 getwd() %>% 
-  str_replace("used-car-liquidity", "diesel-cars/data") %>% 
+  str_replace("used-car-liquidity", "hasznaltauto/data") %>% 
   list.files(full.names = TRUE) %>% 
   walk(~ {
     new_file <- gsub(".*data/", "data/", .)
